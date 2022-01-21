24eeeeeab64aa3ae387ec9fb123537c77a93f604
Louis-Lefebvre <louislefebvre1999@gmail.com>
2022-01-20 19:41:18 -0600

    Hey Joe

--- go/fucking/fucking.go
+++ go/fucking/fucking.go
@@ -24,27 +24,28 @@ func main() {
 	io.Copy(os.Stdout, reader)
 
 	resp, err := cli.ContainerCreate(ctx, &container.Config{
-		Image: "ubuntu",
-		Cmd:   []string{"echo", "hello world"},
-		Tty:   true,
+		Image:       "ubuntu",
+		Tty:         true,
+		AttachStdin: true,
 	}, nil, nil, nil, "")
 	CheckError(err)
 
 	err = cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{})
 	CheckError(err)
 
-	// Take this out to run in the background
-	/*statusCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
-	select {
-	case err := <-errCh:
+	/*
+		// Take this out to run in the background
+		statusCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
+		select {
+		case err := <-errCh:
+			CheckError(err)
+		case <-statusCh:
+		}
+
+		out, err := cli.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true})
 		CheckError(err)
-	case <-statusCh:
-	}
 
-	out, err := cli.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true})
-	CheckError(err)
-
-	stdcopy.StdCopy(os.Stdout, os.Stderr, out)*/
+		stdcopy.StdCopy(os.Stdout, os.Stderr, out)*/
 }
 
 func CheckError(err error) {
