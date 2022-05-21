ec2af362165f868447fc35ffb27d735d3225b21b
Louis-Lefebvre <louislefebvre1999@gmail.com>
2022-05-21 17:04:14 -0500

    log practice

---
+++ projects/side_stuff/logs/log_to_file/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.OpenFile("info.log", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	log.SetOutput(file)
+	log.Println("Logging to a file in Go!")
+}
