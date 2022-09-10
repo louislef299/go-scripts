f91114b62664bc7156341683ef5bebd987267257
Louis-Lefebvre <louislefebvre1999@gmail.com>
2022-09-09 19:17:55 -0500

    add file hash

--- projects/side_stuff/hash/main.go
+++ projects/side_stuff/hash/main.go
@@ -2,13 +2,52 @@ package main
 
 import (
 	"crypto/sha256"
-	"fmt"
+	"io"
+	"log"
+	"os"
 )
 
 
 func main() {
+	basic()
+	file()
+}
+
+func basic() {
 	s := "Hello world"
 
 	hsha2 := sha256.Sum256([]byte(s))
-	fmt.Printf("SHA256: %x\n", hsha2)
+	log.Printf("SHA256: %x\n", hsha2)
+}
+
+func file() {
+	file, err := os.Open("test.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	buf := make([]byte, 30*1024)
+	sha256 := sha256.New()
+	for {
+		n, err := file.Read(buf)
+		if n > 0 {
+			_, err := sha256.Write(buf[:n])
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Printf("Read %d bytes: %v", n, err)
+			break
+		}
+	}
+
+	sum := sha256.Sum(nil)
+	log.Printf("File SHA256: %x\n", sum)
 }
