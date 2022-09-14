984fdb0f510327788eb3ff2cd34a888403405e88
Louis Lefebvre <lefebl4@medtronic.com>
2022-09-14 15:24:37 -0500

    kv project init

---
+++ projects/cloud-native-go/kv/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "errors"
+
+var (
+	store = make(map[string]string)
+
+	ErrorNoSuchKey = errors.New("no such key")
+)
+
+func Put(key, value string) error {
+	store[key] = value
+	return nil
+}
+
+func Get(key string) (string, error) {
+	value, ok := store[key]
+	if !ok {
+		return "", ErrorNoSuchKey
+	}
+	return value, nil
+}
+
+func Delete(key string) error {
+	delete(store, key)
+	return nil
+}
