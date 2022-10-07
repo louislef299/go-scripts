53846ecc0b6f197d94815b2738c46de5c3be7849
Louis-Lefebvre <louislefebvre1999@gmail.com>
2022-10-07 00:41:23 -0500

    interactive ssh session

--- projects/side_stuff/ssh/main.go
+++ projects/side_stuff/ssh/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"bytes"
+	"bufio"
 	"fmt"
 	"log"
+	"os"
+	"strings"
 
 	"golang.org/x/crypto/ssh"
 	"golang.org/x/crypto/ssh/knownhosts"
@@ -20,6 +22,84 @@ import (
  ***/
 
 func main() {
+	fmt.Print("Remote host? (Default=localhost): ")
+	server := scanConfig()
+	if server == "" {
+		server = "127.0.0.1"
+	}
+	fmt.Print("Port? (Default=22): ")
+	port := scanConfig()
+	if port == "" {
+		port = "22"
+	}
+	server = server + ":" + port
+	fmt.Print("UserName?: ")
+	user := scanConfig()
+
+	hostKeyCallback, err := knownhosts.New("/Users/louis/.ssh/known_hosts")
+	if err != nil {
+		log.Fatalf("Unable to create host key call back %v", err)
+	}
+
+	config := &ssh.ClientConfig{
+		User: user,
+		Auth: []ssh.AuthMethod{
+			ssh.Password("test"),
+		},
+		HostKeyCallback: hostKeyCallback,
+	}
+	conn, err := ssh.Dial("tcp", server, config)
+	if err != nil {
+		panic("Failed to dial: " + err.Error())
+	}
+	defer conn.Close()
+
+	// Each ClientConn can support multiple interactive sessions,
+	// represented by a Session.
+	session, err := conn.NewSession()
+	if err != nil {
+		panic("Failed to create session: " + err.Error())
+	}
+	defer session.Close()
+
+	// Set IO
+	session.Stdout = os.Stdout
+	session.Stderr = os.Stderr
+	in, _ := session.StdinPipe()
+
+	// Set up terminal modes
+	modes := ssh.TerminalModes{
+		ssh.ECHO:          0,     // disable echoing
+		ssh.TTY_OP_ISPEED: 14400, // input speed = 14.4kbaud
+		ssh.TTY_OP_OSPEED: 14400, // output speed = 14.4kbaud
+	}
+
+	// Request pseudo terminal
+	if err := session.RequestPty("xterm", 80, 40, modes); err != nil {
+		log.Fatalf("request for pseudo terminal failed: %s", err)
+	}
+
+	// Start remote shell
+	if err := session.Shell(); err != nil {
+		log.Fatalf("failed to start shell: %s", err)
+	}
+
+	// Accepting commands
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		str, _ := reader.ReadString('\n')
+		fmt.Fprint(in, str)
+	}
+
+}
+
+func scanConfig() string {
+	config, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	config = strings.Trim(config, "\n")
+	return config
+}
+
+/*func main() {
 	// ssh config
 	hostKeyCallback, err := knownhosts.New("/Users/louis/.ssh/known_hosts")
 	if err != nil {
@@ -57,7 +137,7 @@ func main() {
 	// start remote shell
 	if err := session.Shell(); err != nil {
 		log.Fatalf("failed to run remote session %v", err)
-	}*/
+	}
 
 	var buff bytes.Buffer
 	session.Stdout = &buff
@@ -65,4 +145,4 @@ func main() {
 		log.Fatalf("failed to send ssh command %v", err)
 	}
 	fmt.Println(buff.String())
-}
+}*/
