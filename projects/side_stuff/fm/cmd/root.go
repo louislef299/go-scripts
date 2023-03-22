4eb8d9017cf25b0287bb4abda6dc2db7cc4b87bf
Louis-Lefebvre <louislefebvre1999@gmail.com>
2023-03-22 15:22:53 -0500

    yaml manipulation

--- projects/side_stuff/fm/cmd/root.go
+++ projects/side_stuff/fm/cmd/root.go
@@ -1,20 +1,25 @@
 /*
 Copyright © 2022 Louis Lefebvre <lefeb073@umn.com>
-
 */
 package cmd
 
 import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
 	"os"
 
 	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v3"
 )
 
-
+const chunkSize = 64000
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "fmxsx",
+	Use:   "fm",
 	Short: "A brief description of your application",
 	Long: `A longer description that spans multiple lines and likely contains
 examples and usage of using your application. For example:
@@ -24,7 +29,25 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+		yfile, err := ioutil.ReadFile("test_files/items.yaml")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		data := make(map[interface{}]interface{})
+		err2 := yaml.Unmarshal(yfile, &data)
+		if err2 != nil {
+			log.Fatal(err2)
+		}
+
+		for k, v := range data {
+			fmt.Printf("%s -> %d\n", k, v)
+		}
+
+		fmt.Println("items and identical are the same:", deepCompare("test_files/items.yaml", "test_files/identical.yaml"))
+		fmt.Println("items and different are the same:", deepCompare("test_files/items.yaml", "test_files/different.yaml"))
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -36,16 +59,40 @@ func Execute() {
 	}
 }
 
-func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
+func deepCompare(file1, file2 string) bool {
+	// Check file size ...
 
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.fmxsx.yaml)")
+	f1, err := os.Open(file1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f1.Close()
 
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	f2, err := os.Open(file2)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f2.Close()
+
+	for {
+		b1 := make([]byte, chunkSize)
+		_, err1 := f1.Read(b1)
+
+		b2 := make([]byte, chunkSize)
+		_, err2 := f2.Read(b2)
+
+		if err1 != nil || err2 != nil {
+			if err1 == io.EOF && err2 == io.EOF {
+				return true
+			} else if err1 == io.EOF || err2 == io.EOF {
+				return false
+			} else {
+				log.Fatal(err1, err2)
+			}
+		}
+
+		if !bytes.Equal(b1, b2) {
+			return false
+		}
+	}
 }
-
-
